695f61a6d578baf8d0754fd5a0bf99f9eb79760e
unknown <mck0391@gmail.com>
2025-11-15 15:45:34 +0000

    icon fixed v1

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,6 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-// Add font file extensions
-config.resolver.assetExts.push('ttf', 'otf', 'woff', 'woff2', 'eot');
 
 config.resolver.unstable_enablePackageExports = true;
 
